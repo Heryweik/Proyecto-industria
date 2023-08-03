a5e6c1ce12d3cd1a07152a8882ad9ee76b05ca71
CarlosGarcia18 <101605732+CarlosGarcia18@users.noreply.github.com>
2023-08-03 11:09:09 -0600

    database

--- backend/config/datebase.php
+++ backend/config/datebase.php
@@ -1,1 +1,11 @@
-.gi
+<?php
+
+$db = mysqli_connect('localhost', 'root', '', 'industria');
+
+
+if (!$db) {
+    echo "Error: No se pudo conectar a MySQL.";
+    echo "errno de depuración: " . mysqli_connect_errno();
+    echo "error de depuración: " . mysqli_connect_error();
+    exit;
+}
