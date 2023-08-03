3d04c01f025afea9b2018b9c7b634150b1d11674
CarlosGarcia18 <101605732+CarlosGarcia18@users.noreply.github.com>
2023-08-03 16:28:52 -0600

    MenuServicio

--- frontend/src/routes/Servicios.jsx
+++ frontend/src/routes/Servicios.jsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer/Footer";
 import servicios from "../assets/img/services.svg";
+import { Link } from "react-router-dom";
 
 
 export default function Servicios() {
@@ -18,8 +19,25 @@ export default function Servicios() {
             <div>
               <h1 style={{ fontWeight: "700" }}>Servicios</h1>
             </div>
-
-          </div>
+        </div>
+        <div className={style.containerMenu}>
+          <Link to={"/servicios"}>
+            <div className={style.card}>
+              <div className={style.img}>
+                <img src={servicios} alt="" />
+              </div>
+                <span className={style.texto}>Servicios</span>
+            </div>
+          </Link>
+          <Link to={"/proveedores"}>
+            <div className={style.card}>
+              <div className={style.img}>
+                <img src={servicios} alt="" />
+              </div>
+                <span className={style.texto}>Proveedores</span>
+              </div>
+          </Link>
+        </div>
       </main>
       <Footer className={style.footer}/>
 
