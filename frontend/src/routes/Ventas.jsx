2314252afceda360adc01ed008b6855c68b2863f
CarlosGarcia18 <101605732+CarlosGarcia18@users.noreply.github.com>
2023-08-03 16:11:46 -0600

    Ventas Commit

--- frontend/src/routes/Ventas.jsx
+++ frontend/src/routes/Ventas.jsx
@@ -3,16 +3,26 @@ import style from "./ventasStyle.module.css";
 import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer/Footer";
-import cliente from "../assets/img/user.svg";
+import ventas from "../assets/img/ventas.svg";
 
 export default function Ventas() {
   return (
     <div className={style.containerFluid}>
       <NavBar/>
       <main className={style.main}>
-      <img src={cliente} alt="" />
+        <div className={style.head}>
+            <div>
+              <img src={ventas} alt="" className={style.img} />
+            </div>
+            <div>
+              <h1 style={{ fontWeight: "700" }}>Ventas</h1>
+            </div>
+
+          </div>
       </main>
       <Footer className={style.footer}/>
+
     </div>
+
   );
 }
