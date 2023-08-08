038647b8fb38e74fee0672515e5ef9f34bb1b194
Yhonny Aplicano <yhonny296@gmail,com>
2023-08-08 13:13:39 -0600

    primera prueba de la PWA

---
+++ frontend/public/service-worker.jsx
@@ -0,0 +1,21 @@
+
+const CACHE_NAME = 'PWA-Industria';
+const urlsToCache = [
+  '/',
+  '../index.html',
+  // Agrega aquí los recursos estáticos que deseas cachear
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
--- frontend/src/routes/Home/Home.jsx
+++ frontend/src/routes/Home/Home.jsx
@@ -2,13 +2,41 @@ import { Link } from "react-router-dom";
 import style from "./homeStyle.module.css";
 import "bootstrap/dist/css/bootstrap.css";
 import logo from "../../assets/img/logoC.jpeg";
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (event) => {
+      event.preventDefault();
+      const installPrompt = event;
+
+      // Mostrar una notificación o botón para instalar la PWA
+      const installButton = document.getElementById('install-button');
+      installButton.style.display = 'block';
+
+      installButton.addEventListener('click', () => {
+        installPrompt.prompt();
+        installButton.style.display = 'none';
+
+        installPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('El usuario aceptó instalar la PWA');
+          } else {
+            console.log('El usuario rechazó la instalación de la PWA');
+          }
+        });
+      });
+    });
+  }, []);
+
   return (
     <div className={style.containerFluid}>
       <div className={style.medium}>
         <div className={style.img}>
           <img src={logo} alt="" className={style.logo} />
+          <button id="install-button" style={{ display: 'none' }}>
+        Instalar PWA
+      </button>
         </div>
       </div>
 
