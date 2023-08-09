69b9e1ecc1fba45623b58cfbdf58dde2b3d6ef75
Yhonny Aplicano <yhonny296@gmail,com>
2023-08-08 19:27:17 -0600

    2.20

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,5 +7,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 80,
+    proxy: {
+      '/deploy': {
+        target: 'http://localhost:80', // Ajusta el puerto si es necesario
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/deploy/, '/deploy.sh')
+      }
+    }
   },
 })
