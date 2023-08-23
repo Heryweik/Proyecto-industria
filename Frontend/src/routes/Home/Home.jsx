e3aeba282bb4aee23d4a434b4d8d85b351908ccb
Yhonny Aplicano <yhonny296@gmail,com>
2023-08-22 23:07:58 -0600

    final

--- Frontend/src/routes/Home/Home.jsx
+++ Frontend/src/routes/Home/Home.jsx
@@ -2,27 +2,27 @@ import { Link } from "react-router-dom";
 import style from "./homeStyle.module.css";
 import "bootstrap/dist/css/bootstrap.css";
 import logo from "../../assets/img/logoC.jpeg";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    window.addEventListener('beforeinstallprompt', (event) => {
+    window.addEventListener("beforeinstallprompt", (event) => {
       event.preventDefault();
       const installPrompt = event;
 
       // Mostrar una notificación o botón para instalar la PWA
-      const installButton = document.getElementById('install-button');
-      installButton.style.display = 'block';
+      const installButton = document.getElementById("install-button");
+      installButton.style.display = "block";
 
-      installButton.addEventListener('click', () => {
+      installButton.addEventListener("click", () => {
         installPrompt.prompt();
-        installButton.style.display = 'none';
+        installButton.style.display = "none";
 
         installPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('El usuario aceptó instalar la PWA');
+          if (choiceResult.outcome === "accepted") {
+            console.log("El usuario aceptó instalar la PWA");
           } else {
-            console.log('El usuario rechazó la instalación de la PWA');
+            console.log("El usuario rechazó la instalación de la PWA");
           }
         });
       });
@@ -32,22 +32,16 @@ export default function Home() {
   return (
     <div className={style.containerFluid}>
       <div id="install-button" className={style.pwa}>
-      <button  className={style.pwaB}>
-        Instalar aplicacion
-      </button>
+        <button className={style.pwaB}>Instalar aplicacion</button>
       </div>
-      
+
       <div className={style.medium}>
         <div className={style.img}>
           <img src={logo} alt="" className={style.logo} />
-          
         </div>
       </div>
 
-      <div
-        className={`${style.medium} ${style.bienvenido}`}
-      >
-      
+      <div className={`${style.medium} ${style.bienvenido}`}>
         <div className={style.c1}></div>
         <div className={style.c2}></div>
         <div className={style.c3}></div>
@@ -69,10 +63,10 @@ export default function Home() {
             <button className={style.iSesion}>Registrarse</button>
           </Link>
         </div>
-        
-      <div className={style.footer}>
-        <p>@YhonnyAplicano_2023</p>
-      </div>
+
+        <div className={style.footer}>
+          <p>@YhonnyAplicano_2023</p>
+        </div>
       </div>
     </div>
   );
